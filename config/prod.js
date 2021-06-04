16ce7b1726581b08a4d48880d636a29dc30843bf
paulthechampion <enwerempaulo@gmail.com>
2021-06-04 01:34:10 -0400

    prod keys typo

--- config/prod.js
+++ config/prod.js
@@ -10,5 +10,5 @@ module.exports ={
     stripePublishableKey : process.env.STRIPE_PUBLISHABLE_KEY,
     stripeSecretKey: process.env.STRIPE_SECRET_KEY,
     sendGridKey: process.env.SEND_GRID_KEY,
-    redirectDomain:proces.env.REDIRECT_DOMAIN
+    redirectDomain:process.env.REDIRECT_DOMAIN
 }
